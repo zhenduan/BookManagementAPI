018da09d17171c20f5851357247a79aa49c2cdf5
zhenduan <duan2439@gmail.com>
2025-02-21 15:36:44 +1100

    remove code

--- Program.cs
+++ Program.cs
@@ -123,7 +123,3 @@ app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
